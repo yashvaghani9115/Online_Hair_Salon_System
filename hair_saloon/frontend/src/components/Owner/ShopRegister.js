cffd36bf7142badecc4537d5cda9cabf7381eccf
yashshah935 <yashshah935@gmail.com>
2022-01-29 13:09:59 +0530

    shopRegister Bug Fix

--- hair_saloon/frontend/src/components/Owner/ShopRegister.js
+++ hair_saloon/frontend/src/components/Owner/ShopRegister.js
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import TimeField from 'react-simple-timefield';
 import './ShopRegister.css';
-import MapPicker from 'react-google-map-picker'
+import MapPicker from 'react-google-map-picker';
+import { Modal,Button } from 'react-bootstrap';
 
 const DefaultLocation = { lat: 21.101472400442564, lng: 72.82393134493594 };
 const DefaultZoom = 10;
 function ShopRegister() {
     const history = useHistory();
+    const [show,setShow] = useState(false);
+    const [header,setHeader] = useState("");
+
+    const [msg,setMsg] = useState("");
 
     const [shop, setShop] = useState({
         shop_name: "",
@@ -16,14 +21,13 @@ function ShopRegister() {
         closing_time: "",
         salon_gender_type: "",
         capacity_seats: "",
-        verified: false,
-        owner_id: 0,
-        longitude: 0.0,
-        latitude: 0.0
+        verified: "",
+        longitude: 72.82393134493594,
+        latitude: 21.101472400442564
     })
     const [defaultLocation, setDefaultLocation] = useState(DefaultLocation);
 
-  const [location, setLocation] = useState(defaultLocation);
+    // const [location, setLocation] = useState(defaultLocation);
 
     const [agree, setAgree] = useState(false);
     const [zoom, setZoom] = useState(DefaultZoom);
@@ -31,7 +35,9 @@ function ShopRegister() {
         setAgree(!agree);
     }
     function handleChangeLocation(lat, lng) {
-        setLocation({ lat: lat, lng: lng });
+        // setLocation({ lat: lat, lng: lng });
+        setShop({ ...shop, longitude: lng , latitude: lat });
+
     }
 
     function handleChangeZoom(newZoom) {
@@ -39,20 +45,14 @@ function ShopRegister() {
     }
 
     function handleResetLocation() {
-        setDefaultLocation({ ...DefaultLocation });
-        setLocation({ ...DefaultLocation });
+        // setDefaultLocation({ ...DefaultLocation });
+        getLocation();
+        // setShop({...shop,longitude:defaultLocation.lng,latitude:defaultLocation.lat})
+        // setLocation({ ...DefaultLocation });
         setZoom(DefaultZoom);
     }
-    function setLocationinstate(){
-        setShop({ ...shop, longitude: location.lng , latitude: location.lat });
-        console.log("setloc called");
-    }
-    // function handleLocationChange({ position, address, places }) {
 
-    //     // Set new location
-    //     setPosition(position);
-    //     setAddress(address);
-    // }
+   
     function handlechange(e) {
         const { name, value } = e.target;
 
@@ -63,39 +63,22 @@ function ShopRegister() {
             }
         )
     }
-    // function getLocation() {
-    //     if (navigator.geolocation) {
-    //         navigator.geolocation.getCurrentPosition(getPosInState);
-    //     } else {
-    //         //   x.innerHTML = "Geolocation is not supported by this browser.";
-    //     }
-    // }
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(getPosInState);
+        } 
+    }
 
-    // function getPosInState(position) {
-    //     // x.innerHTML = "Latitude: " + position.coords.latitude + 
-    //     // "<br>Longitude: " + position.coords.longitude;
+    function getPosInState(position) {
+        setDefaultLocation({lat: position.coords.latitude , lng: position.coords.longitude} )
+        setShop({...shop,longitude:position.coords.longitude,latitude:position.coords.latitude})
 
-    //     setShop({ ...shop, longitude: position.coords.longitude, latitude: position.coords.latitude })
 
-    //     // return position;
-    // }
+    }
     function handlegender(e) {
         setShop({ ...shop, salon_gender_type: e.target.value });
     }
-    // async function getOwnerId() {
-    //     const ownerId = await JSON.parse(localStorage.getItem("owner"))._id;
-    //     console.log(ownerId);
-    //     console.log("getowner called");
-    //     setShop({ ...shop, owner_id: ownerId });
-    //     // while(ownerId!=0){
-    //     //     setShop({ ...shop, owner_id: ownerId });
-    //     // }
-    // }
 
-    // useEffect(() => {
-    //     getLocation();
-    //     // getOwnerId();
-    // }, []);
     async function registerShop() {
 
         const {
@@ -105,12 +88,11 @@ function ShopRegister() {
             closing_time,
             salon_gender_type,
             capacity_seats,
-            owner_id,
             longitude,
             latitude
         } = shop;
         console.log(shop);
-        if (shop_name, address, opening_time, closing_time, salon_gender_type, capacity_seats, owner_id, longitude, latitude) {
+        if (shop_name, address, opening_time, closing_time, salon_gender_type, capacity_seats, longitude, latitude) {
             var res = await fetch("http://localhost:9700/owner/addShop", {
                 method: "POST",
                 headers: {
@@ -124,13 +106,10 @@ function ShopRegister() {
                     closing_time: closing_time,
                     salon_gender_type: salon_gender_type,
                     capacity_seats: capacity_seats,
-                    verified: false,
+                    verified: "pending",
                     owner_id: await JSON.parse(localStorage.getItem("owner"))._id,
                     longitude: longitude,
                     latitude: latitude
-                    // owner_id:150,
-                    // latitude:10,
-                    // longitude:5
                 })
             })
 
@@ -173,9 +152,9 @@ function ShopRegister() {
                             </div>
                             <div className="col-6 pt-5">
                                 <label className='label1'>Saloon Gender type <span style={{ color: "red" }}> *</span></label><br />
-                                <input type="radio" className='radio' value="M" onChange={handlegender} name="salon_gender_type" /> <label className='label'>Male</label>
-                                <input type="radio" style={{ marginLeft: "15px" }} className='radio' value="F" onChange={handlegender} name="salon_gender_type" /> <label className='label'>Female</label>
-                                <input type="radio" style={{ marginLeft: "15px" }} className='radio' value="B" onChange={handlegender} name="salon_gender_type" /> <label className='label'>Both</label>
+                                <input type="radio" className='radio' value="Male" onChange={handlegender} name="salon_gender_type" /> <label className='label'>Male</label>
+                                <input type="radio" style={{ marginLeft: "15px" }} className='radio' value="Female" onChange={handlegender} name="salon_gender_type" /> <label className='label'>Female</label>
+                                <input type="radio" style={{ marginLeft: "15px" }} className='radio' value="Both" onChange={handlegender} name="salon_gender_type" /> <label className='label'>Both</label>
                             </div>
                             <div className="col-6 pt-5">
                                 <label className='label1'>Capacity <span style={{ color: "red" }}> *</span></label><br />
@@ -192,46 +171,61 @@ function ShopRegister() {
                             <div className='col-6 pt-5'>
                                 
                                 <label className='label1'>Select Location <span style={{ color: "red" }}> *</span></label><br />
-                                <label className='label1'>Latitute: </label><input className='text-box1' type='text' value={location.lat} disabled /> &nbsp;<br/>
-                                <label className='label1'>Longitute: </label><input type='text' className='text-box1' value={location.lng} disabled />&nbsp;<br/>
+                                <label className='label1'>Latitute: </label><input className='text-box1' type='text' value={shop.latitude} disabled /> &nbsp;<br/>
+                                <label className='label1'>Longitute: </label><input type='text' className='text-box1' value={shop.longitude} disabled />&nbsp;<br/>
+                                <Button onClick={()=>{setShow(true);setMsg();setHeader()}} >Location</Button>
+                                
                             </div>
+                            
                             <div className='col-6 pt-5'>
                                 <label className='label1'>Verify Your account: </label><br />
                                 <input type="checkbox" onChange={handlecheckbox} /><label style={{marginLeft:"10px"}} htmlFor="agree"> I verify my <b>credentials for shop </b> and <b>location</b> <span style={{color:"red"}}>*</span></label><br/>
-                                <button className='button' type="submit" disabled={!agree} onClick={()=>{registerShop();setLocationinstate();}}>Verify</button>
+                                <button className='button' type="submit" disabled={!agree} onClick={()=>{registerShop()}}>Verify</button>
                             </div>
+                            
                         </div>
                     </div>
 
                 </div>
             </div>
             <div className='location'>
-                {/* <div>
-                    <h1>{address}</h1>
-                    <div>
-                        <LocationPicker
-                            containerElement={<div style={{ height: '100%' }} />}
-                            mapElement={<div style={{ height: '400px' }} />}
-                            defaultPosition={defaultLocation}
-                            onChange={handleLocationChange}
-                            zoom={zoom}
-                        />
-                    </div>
-                </div> */}
-                <div className='under_loc'>
-                <button className='btn btn-primary btn-sm'  onClick={handleResetLocation}>Reset Location</button> &nbsp;
-                <label className='label1'>Latitute: </label><input className='text-box1' type='text' value={location.lat} disabled /> &nbsp;
-                <label className='label1'>Longitute:</label><input type='text' className='text-box1' value={location.lng} disabled />&nbsp;
-                <label className='label1'>Zoom:</label><input type='text' className='text-box1' value={zoom} disabled />
-                </div>
-                <MapPicker defaultLocation={defaultLocation}
-                    zoom={zoom}
-                    containerElement={<div style={{ height: '100%' }} />}
-                    mapTypeId="roadmap"
-                    style={{ height: '700px' }}
-                    onChangeLocation={handleChangeLocation}
-                    onChangeZoom={handleChangeZoom}
-                    apiKey='AIzaSyD07E1VvpsN_0FvsmKAj4nK9GnLq-9jtj8' />
+                
+                
+                    <>
+                    <Modal
+                    size="xl"
+                    show={show}
+                    onHide={() => setShow(false)}
+                    // onclick={handleResetLocation}
+                    
+                >
+                    <Modal.Header closeButton>
+                    <Modal.Title id="example-modal-sizes-title-sm">
+                        <div style={{textAlign:"center"}}>
+                            <button className='btn btn-primary btn-sm' onClick={handleResetLocation} >Set Current Location</button> 
+                            </div>
+                            <div>
+                        <label className=''>Latitute: </label><input className='' type='text' value={shop.latitude} disabled /> 
+                        <label className=''>Longitute:</label><input type='text' className='' value={shop.longitude} disabled />
+                        <label className=''>Zoom:</label><input type='text' className='' value={zoom} disabled />
+                        </div>
+                    </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className='bg-light'>
+                        <MapPicker defaultLocation={defaultLocation}
+                        zoom={zoom}
+                        containerElement={<div style={{ height: '100%' }} />}
+                        mapTypeId="roadmap"
+                        style={{ height: '700px' }}
+                        onChangeLocation={handleChangeLocation}
+                        onChangeZoom={handleChangeZoom}
+                        apiKey='AIzaSyD07E1VvpsN_0FvsmKAj4nK9GnLq-9jtj8' />
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button style={{borderRadius:"20px"}} onClick={()=>setShow(false)}>Done</Button>
+                    </Modal.Footer>
+                </Modal>
+                    </>
             </div>
         </>
 
